9acaeecc3d157e6eb96a2bcf2d506f91fd6ab634
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2025-03-06 10:47:38 +0530

    remove some comments

--- Script.js
+++ Script.js
@@ -1,2 +1,30 @@
-console.log("hello World ");   
- 
+
+
+const Hours=document.querySelector(".hours");
+const Minutes=document.querySelector(".minutes");
+const Seconds=document.querySelector(".seconds");
+const AmPm=document.querySelector(".ampm");
+
+
+
+function displayTime(){
+    const dataTime=new Date()
+    let hr =dataTime.getHours(); 
+    let min =dataTime.getMinutes();
+    let sec =dataTime.getSeconds();
+    if(hr>12){
+        hr = hr - 12;
+        AmPm.innerHTML='PM'
+    }
+    Hours.innerHTML = padZero(hr);
+    Minutes.innerHTML = padZero(min);
+    Seconds.innerHTML = padZero(sec);
+}
+
+function padZero(num){
+    return num>10?num:"0"+num; 
+}
+setInterval(displayTime,500);
+
+
+
